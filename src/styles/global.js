09436169068402fd5e749cd530f28c69a1744e7a
GustavoPec <gustavo@softilux.com.br>
2023-06-15 22:32:33 -0300

    Ajuste de fonte

--- src/styles/global.js
+++ src/styles/global.js
@@ -8,17 +8,19 @@ export const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
 }
 
-/* @font-face {
+@font-face {
   font-family: 'GOBOLD';
-  src: font-url('../assets/Gobold Bold.otf') format('opentype');
-} */
+  src: font-url('../assets/Gobold Bold.otf') format('opentype'),
+   font-url('../assets/Gobold Bold.otf') format('woff'),
+   font-url('../assets/Gobold Bold.otf') format('truetype');
+}
 
-@font-face{
+/* @font-face{
     font-family:"GOBOLD";
     src: font-url("https://candyfonts.com/wp-data/2019/03/22/41942/GoboldBold.ttf") format("woff"),
     font-url("https://candyfonts.com/wp-data/2019/03/22/41942/GoboldBold.ttf") format("opentype"),
     font-url("https://candyfonts.com/wp-data/2019/03/22/41942/GoboldBold.ttf") format("truetype");
-}
+} */
 
     body{
             margin: 0;
