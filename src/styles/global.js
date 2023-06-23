5597e25b6f059ca6c9ec39ea037c8b5e8b6e3b15
GustavoPec <gustavo@softilux.com.br>
2023-06-23 11:16:40 -0300

    Update global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -12,6 +12,7 @@ export const GlobalStyle = createGlobalStyle`
     body{
             margin: 0;
             padding: 0;
+            background: #f7f7f7;
         }
 
         body, input, textarea, button{
